650fa4650e9e1ffe1a5439e5fd782fd2927611fd
Evgenii Kievsky <evgenii.kievsky@gmail.com>
2024-08-16 12:09:01 +0200

    Initial commit

---
+++ ReaddleRepoSearch/ReaddleRepoSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReaddleRepoSearchApp.swift
+//  ReaddleRepoSearch
+//
+//  Created by Yevhen Kyivskyi on 16.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ReaddleRepoSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
